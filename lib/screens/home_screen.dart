dc1f509c7a6ebb7ba0cb381ba6ae7ac8fbc237eb
Lucas Fachinetto <67347999+lfachinetto@users.noreply.github.com>
2020-08-21 17:01:12 -0300

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/about_tab.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/my_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
+import 'package:loja_virtual/tabs/places_tab.dart';
+import 'package:loja_virtual/tabs/process_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
+import 'package:loja_virtual/tabs/scan_tab.dart';
+import 'package:loja_virtual/tabs/search_tab.dart';
+import 'package:loja_virtual/tabs/start_tab.dart';
+import 'package:loja_virtual/widgets/cart_button.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(), //Para não ser scrollable
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Evidences3D"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: StartTab(),
+          //floatingActionButton: CartButton(),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Escanear evidência"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ScanTab(),
+          //floatingActionButton: CartButton(),
+        ),
+        Scaffold(
+            appBar: AppBar(
+                title: Text("Processamento"),
+                centerTitle: true
+            ),
+            body: ProcessTab(),
+            drawer: CustomDrawer(_pageController)
+        ),
+        Scaffold(
+            appBar: AppBar(
+                title: Text("Minhas evidências"),
+                centerTitle: true
+            ),
+            body: MyTab(),
+            drawer: CustomDrawer(_pageController)
+        ),
+        Scaffold(
+            appBar: AppBar(
+                title: Text("Buscar evidência"),
+                centerTitle: true
+            ),
+            body: SearchTab(),
+            drawer: CustomDrawer(_pageController)
+        ),
+        Scaffold(
+            appBar: AppBar(
+                title: Text("Sobre"),
+                centerTitle: true
+            ),
+            body: AboutTab(),
+            drawer: CustomDrawer(_pageController)
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/tabs/process_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProcessTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Text("Status de processamento, no estilo do meus pedidos")
+      ],
+    );
+  }
+}
